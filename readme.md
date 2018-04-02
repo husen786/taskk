a48fbfd933c762524e5e922649c8a9900620445f
Rahat husen <husensrk@gmail.com>
2018-04-02 13:29:31 +0530

    task
    task management portal

---
+++ readme.md
@@ -0,0 +1,39 @@
+## About Task Manage
+
+Task Manage is a web application build using Laravel & VueJS frameworks.
+
+## Installation and use
+
+```
+$ git clone https://github.com/hrshadhin/task-manage.git
+```
+```
+$ cd task-manage
+```
+```
+$ mv .env.example .env
+```
+**Change configuration according your need and create Database**
+```
+$ composer install
+```
+```
+$ php artisan migrate
+```
+```
+$ php artisan serve
+```
+**  http://localhost:8000 **
+
+##Demo
+```
+http://task.hrshadhin.me
+```
+## Screenshots
+<img src="public/screenshots/1.png" alt="">
+<img src="public/screenshots/2.png" alt="">
+<img src="public/screenshots/3.png" alt="">
+
+## License
+Task Manage is open-sourced software licensed under the [GPL-3.0 license](https://opensource.org/licenses/GPL-3.0).
+Frameworks and libraries has it own licensed
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Task Management Portal</title>
+    
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+               
+                font-size: 50px;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+            .links > a{
+                color: #3097D1;
+            }
+
+            ul li{
+                text-decoration: none;
+                display: inline-block;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+
+                        <a href="{{ url('/home') }}">
+                        <span class="glyphicon glyphicon-home"></span>
+                        Home
+                        </a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
+
+           <div class="content">
+            <h4 style="color:#3097D1;">Welcome to your personal Task Management System</h4>
+            
+            
+            <p style="color:#636b6f;font-size: 30px;">A task management system is the process of managing the tasks which involves the creation,updation and deletion of the tasks.In this task management system i have created user registration and login system where new user can register themselves.Once the user gets login with valid email and password ,it is redirected to home page and can access the task management functionality.</p>
+            
+           </div> 
+    </body>
+</html>
